d35a78e40bc57564cbc6d563a9f30a75cf7624d7
Mike <m.reissig-it.de>
2022-10-28 15:21:20 +0200

    Initial commit

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		await Navigation.PushModalAsync(new Page1());
+	}
+}
+
